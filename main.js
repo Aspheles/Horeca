27560cc5b331204a5bee0e379c7512b40abc8bb0
Yavuz <yavuzkaand@hotmail.com>
2018-11-01 18:23:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var chart = [];
 
 
+
 var products = {
     Fris : { productName: "Fris", price: 2 },
     Bier : { productName: "Bier", price: 5 },
@@ -8,22 +9,29 @@ var products = {
 }
 
 function takeOrder() {
-    while(order !== "stop"){
+    while(order !=="stop"){   
     var order = prompt("What product do you want to add to your chart (Fris, Bier or Wijn) type stop to finish your order");
+    if(order==="stop"){
+
+        break;
+    }
     var amount = prompt("How many " + products[order].productName + " do you want to add to your chart");
     if (Object(products).hasOwnProperty(order)) {
         for (i = 0; i < amount; i++) {
             chart.push(products[order]);
         }
-
     }
+    
 }
     console.log(chart);
-    console.log("$ " + amount * products[order].price);
+   
+   
+    
     
 
 }
 
 
 takeOrder();
+
 
